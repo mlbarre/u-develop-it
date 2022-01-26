fc654cb7ca4ee90a3bdf771baa85a61c09159715
mlbarre <mlbarrera1538@gmail.com>
2022-01-26 13:26:25 -0600

    connected to mysql

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // mysql username
+        user: 'root',
+        // mysql password
+        password: '0921%mlb',
+        database: 'election'
+    },
+    console.log('connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response to any other request - not found
 app.use((req, res) => {
